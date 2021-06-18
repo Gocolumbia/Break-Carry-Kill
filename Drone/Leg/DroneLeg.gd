f93714103eeb86b42abd0bacf4730268be19a150
Gocolumbia <wkoyama109@gmail.com>
2021-06-18 03:35:25 -0700

    Walking Drone WIP

---
+++ Drone/Leg/DroneLeg.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	$AnimationPlayer.play("LegRaise")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Leg.scale.y = $FootDownPos.position.y / -100
+	$Colored/Foot.position.y = $FootDownPos.position.y + 78
+#	global_rotation = $FootDownPos.position.angle()
+	pass
